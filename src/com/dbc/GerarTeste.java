2b0b4fab0bfe7774748d72a1ceb156e332c81051
rosa_mario <rosa_mario@sicredi.com.br>
2021-08-20 10:18:37 -0300

    melhoria filtragem de alguns imports e adicao de testes de outras tipagens.

--- src/com/dbc/GerarTeste.java
+++ src/com/dbc/GerarTeste.java
@@ -5,13 +5,13 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.reflect.Field;
+import java.lang.reflect.Type;
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.net.URL;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class GerarTeste {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
@@ -27,26 +27,47 @@ public class GerarTeste {
                             "import org.junit.runner.RunWith;\n" +
                             "import org.mockito.junit.MockitoJUnitRunner;\n" +
                             "\n" +
-                            "import java.time.LocalDateTime;\n" +
-                            "import java.math.BigDecimal;\n" +
-                            "import java.util.ArrayList;\n" +
-                            "\n" +
-                            "import static org.junit.Assert.*;\n" +
-                            "\n" +
-                            "@RunWith(MockitoJUnitRunner.class)\n" +
-                            "public class " + simpleName + "Test {\n" +
-                            "    @Test\n" +
-                            "    public void test() {").append(System.lineSeparator());
+                            "import static org.junit.Assert.*;\n"
+            );
+            Set<Type> filtrados = new HashSet<>();
+            int tamanhoAnterior = 0;
+            for(Field field : allFields){
+                filtrados.add(field.getType());
+                if(filtrados.size() > tamanhoAnterior){
+                    if (field.getType() == LocalDateTime.class) {
+                        testeGerado.append("import java.time.LocalDateTime;\n");
+                    }else if (field.getType() == LocalDate.class) {
+                        testeGerado.append("import java.time.LocalDate;\n");
+                    }else if (field.getType() == List.class) {
+                        testeGerado.append("import java.util.List;\n");
+                    }else if (field.getType() == ArrayList.class) {
+                        testeGerado.append("import java.util.ArrayList;\n");
+                    }else if (field.getType() == Set.class) {
+                        testeGerado.append("java.util.Set;\n");
+                    }else if (field.getType() == BigDecimal.class) {
+                        testeGerado.append("import java.math.BigDecimal;\n");
+                    }else if (field.getType() == BigInteger.class) {
+                        testeGerado.append("import java.math.BigInteger;\n");
+                    }
+                    tamanhoAnterior++;
+                }
+            }
 
-            testeGerado.append("        LocalDateTime now = LocalDateTime.now();").append(System.lineSeparator());
+            testeGerado.append("\n" +
+                    "@RunWith(MockitoJUnitRunner.class)\n" +
+                    "public class " + simpleName + "Test {\n" +
+                    "    @Test\n" +
+                    "    public void test() {").append(System.lineSeparator());
             testeGerado.append("        " + simpleName + " response = new " + clazz.getSimpleName() + "();").append(System.lineSeparator());
-            testeGerado.append("        assertNotNull(response);").append(System.lineSeparator());
             for (Field field : allFields) {
                 final String name = field.getName();
                 String cap = name.substring(0, 1).toUpperCase() + name.substring(1);
                 if (field.getType() == String.class) {
                     testeGerado.append("        response.set" + cap + "(\"teste\");").append(System.lineSeparator());
                     testeGerado.append("        assertEquals(\"teste\", response.get" + cap + "());").append(System.lineSeparator());
+                }else if (field.getType() == Character.class) {
+                    testeGerado.append("        response.set" + cap + "('A');").append(System.lineSeparator());
+                    testeGerado.append("        assertNotNull(response.get" + cap + "());").append(System.lineSeparator());
                 } else if (field.getType() == Double.class) {
                     testeGerado.append("        response.set" + cap + "(0D);").append(System.lineSeparator());
                     testeGerado.append("        assertEquals(0D, response.get" + cap + "(), 1e-15);").append(System.lineSeparator());
@@ -57,8 +78,11 @@ public class GerarTeste {
                     testeGerado.append("        response.set" + cap + "(1);").append(System.lineSeparator());
                     testeGerado.append("        assertEquals(1, response.get" + cap + "().intValue());").append(System.lineSeparator());
                 } else if (field.getType() == LocalDateTime.class) {
-                    testeGerado.append("        response.set" + cap + "(now);").append(System.lineSeparator());
-                    testeGerado.append("        assertEquals(now, response.get" + cap + "());").append(System.lineSeparator());
+                    testeGerado.append("        response.set" + cap + "(LocalDateTime.now());").append(System.lineSeparator());
+                    testeGerado.append("        assertNotNull(response.get" + cap + "());").append(System.lineSeparator());
+                }else if (field.getType() == LocalDate.class) {
+                    testeGerado.append("        response.set" + cap + "(LocalDate.now());").append(System.lineSeparator());
+                    testeGerado.append("        assertNotNull(response.get" + cap + "());").append(System.lineSeparator());
                 } else if (field.getType() == List.class) {
                     testeGerado.append("        response.set" + cap + "(new ArrayList<>());").append(System.lineSeparator());
                     testeGerado.append("        assertEquals(0, response.get" + cap + "().size());").append(System.lineSeparator());
@@ -68,7 +92,11 @@ public class GerarTeste {
                 }else if (field.getType() == BigDecimal.class) {
                     testeGerado.append("        response.set" + cap + "(new BigDecimal(1L));").append(System.lineSeparator());
                     testeGerado.append("        assertEquals(1L, response.get" + cap + "().longValue());").append(System.lineSeparator());
-                } else if (field.getType() == Boolean.class) {
+                }else if (field.getType() == BigInteger.class) {
+                    testeGerado.append("        response.set" + cap + "(BigInteger.ONE);").append(System.lineSeparator());
+                    testeGerado.append("        assertNotNull(response.get" + cap + "());").append(System.lineSeparator());
+                }
+                else if (field.getType() == Boolean.class) {
                     testeGerado.append("        response.set" + cap + "(Boolean.TRUE);").append(System.lineSeparator());
                     testeGerado.append("        assertTrue(response.get" + cap + "());").append(System.lineSeparator());
                 } else if (field.getType().isEnum()) {
@@ -83,7 +111,7 @@ public class GerarTeste {
             }
             testeGerado.append("   }\n" +
                     "}\n");
-            final String fileName = "C:\\classes\\" + simpleName + "Test.java";
+            final String fileName = "C:\\Users\\Desktop\\arq Test\\classes\\" + simpleName + "Test.java";
             File file = new File(fileName);
             if (!file.exists()) {
                 file.createNewFile();
